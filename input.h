416a6d20a6bb088cc1461c5461b74e937271c0a7
nwon24 <nwonb@schools.vic.edu.au>
2020-12-18 22:51:07 +1100

    added comment about CTRL macro

--- input.h
+++ input.h
@@ -1,6 +1,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/* Following macros bitwise ANDs a character with 0x1f
+ * which is what happens when you hold down the control
+ * key with another character. This allows us to define
+ * and map C-* like key bindings to different editor functions
+ */
+
 #ifndef CTRL
 #define CTRL(key) ((key) & 0x1f)
 #endif
