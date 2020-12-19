fc2cee897ffac0e10ecd423c257c3ba24eaf36d3
nwon24 <nwonb@schools.vic.edu.au>
2020-12-19 14:17:42 +1100

    implmented inserting and deleting chars

--- edit.c
+++ edit.c
@@ -4,6 +4,8 @@
 
 #include "edit.h"
 
+extern struct window win;
+
 /* The following function inserts a single
  * character into string s of a row struct at position pos
  */
@@ -11,7 +13,7 @@
 void insert_char(struct row *erow, int pos, char c)
 {
 	/* First, we need to allocate enough memory */
-	erow->s = realloc(erow->s, erow->size + 1);
+	erow->s = realloc(erow->s, erow->size + 2);
 	int i;
 	
 	/* Now we loop backwards over the string to shift
@@ -19,14 +21,14 @@ void insert_char(struct row *erow, int pos, char c)
 	 * character
 	 */
 	
-	for (i = erow->size; i >= pos; i--)
+	for (i = erow->size; i > pos; i--)
 		erow->s[i] = erow->s[i - 1];
 	erow->s[pos] = c;
 	erow->size++;
-	erow->s[erow->size] = '\0';
+	erow->s[erow->size] = '\0'; /* reinsert terminating char */
 }
 
-void del_char(struct row *erow, int pos, char c)
+void del_char(struct row *erow, int pos)
 {
 	int i;
 	
@@ -44,4 +46,28 @@ void del_char(struct row *erow, int pos, char c)
 	 */
 	
 	erow->s = realloc(erow->s, erow->size);
+}
+
+/* This function writes the contents of the array
+   of row structs into a file. If the file name parameter specified
+   is the original file name, then the file is simply being saved. If it
+   is a different name, then the file is being saved with a new name (equivalent
+   of a save as) */
+   
+void write_to_disk(char *name)
+{
+	FILE *fp = fopen(name, "w");
+	/* We need no error checking here because opening it with write mode
+	   creates the file if it is not there */
+	   
+	int i;
+	
+	/* Now we loop over the array of row structs and write each of them into
+	   the file, adding a newline character after each one. */
+	   
+	for (i = 0; i <= win.numrows - 1; i++) {
+		fputs( win.rows[i].s, fp);
+		fputs("\n", fp);
+	}
+	fclose(fp);
 }
--- edit.h
+++ edit.h
@@ -1,6 +1,8 @@
 #include <string.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "estruct.h"
 
 void insert_char(struct row *erow, int pos, char c);
-void del_char(struct row *erow, int pos, char c);
+void del_char(struct row *erow, int pos);
+void write_to_disk(char *name);
