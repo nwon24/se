94a394b4e9d4a0a0faf81d7c4c5b0fc749ba7c8b
nwon24 <nwonb@schools.vic.edu.au>
2021-01-01 14:37:56 +1100

    updated headers and function declarations

--- file.h
+++ file.h
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
+struct stat fs;
 
 char *fgetline(char *name, int pos);
 int fexist(char *file);
 int get_nrow(char *file);
 void read_into_struct(char *file);
-
-
+void init_file();
