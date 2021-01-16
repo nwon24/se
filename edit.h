29018bbe82be069e54a059808abab0093114a3a7
nwon24 <nwonb@schools.vic.edu.au>
2021-01-16 14:23:00 +1100

    Updated function declarations

--- edit.h
+++ edit.h
@@ -10,3 +10,6 @@ void new_line(char *s, size_t len, int pos);
 char *split_line(struct row *erow, int pos);
 void del_line(int pos);
 void append_line(char *s);
+void cut_line(int pos);
+void copy_line(int pos);
+int put_line(int pos);
