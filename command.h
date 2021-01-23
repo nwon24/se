92bb52858e08bb20796e41ced611e53c68d8aa90
nwon24 <nwonb@schools.vic.edu.au>
2021-01-24 10:38:17 +1100

    command.c header file

---
+++ command.h
@@ -0,0 +1,6 @@
+/* command_mode similar to vim
+   Users are given the choice; they can stay in insert mode and
+   stay modless or switch between modes to avoid getting hand injuries
+   from Emacs-like key bindings */
+
+void command_mode();
