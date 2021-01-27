84a8612b15931a1fb89cd8ceae88381ae037a869
nwon24 <nwonb@schools.vic.edu.au>
2021-01-27 14:21:02 +1100

    Updated header file for tabs

--- edit.h
+++ edit.h
@@ -3,6 +3,11 @@
 #include <stdio.h>
 #include "estruct.h"
 
+#define TAB_SIZE 8
+#define NO_TAB 0
+#define BEG_TAB 1
+#define END_TAB 2
+
 void insert_char(struct row *erow, int pos, char c);
 void del_char(struct row *erow, int pos);
 void write_to_disk(char *name);
@@ -13,3 +18,5 @@ void append_line(char *s);
 void cut_line(int pos);
 void copy_line(int pos);
 int put_line(int pos);
+int check_tab(struct row *erow, int pos);
+void del_tab();
